18aa7aa9555231662ce85bbf63717ae260ff5206
S_A_M <108063107+SAMAKSH10@users.noreply.github.com>
2023-09-08 19:26:49 +0530

    update

--- votingmanagementsystem/home/views.py
+++ votingmanagementsystem/home/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 
 # Create your views here.
 
 def homepage(request):
-    template="hello homepage !"
-    return HttpResponse(template)
+    template=loader.get_template('homepage.html')
+    return HttpResponse(template.render())
--- votingmanagementsystem/login/views.py
+++ votingmanagementsystem/login/views.py
@@ -3,5 +3,5 @@ from django.template import loader
 from django.http import HttpResponse
 
 def loginpage(request):
-    template="Heloo world!"
-    return HttpResponse(template)
+    template=loader.get_template('login.html')
+    return HttpResponse(template.render())
